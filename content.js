ab99e265c3ec51aa93883f4fe667034d0677d263
marcosocon <oconmarcos97@gmail.com>
2018-01-24 03:52:49 -0300

    added clarin modal by pass

---
+++ content.js
@@ -0,0 +1,26 @@
+let intervalFunc;
+
+function checkClarinModalVisibility() {
+  let tryCount = 0;
+  let arrayOfModals = document.querySelectorAll('.modal-pase');
+  let lockedElement = document.querySelector('.lock');
+
+  if (tryCount === 5) {
+    console.log('trycount', tryCount);
+    return;
+  }
+
+  if (arrayOfModals.length) {
+    arrayOfModals.forEach((blocker) => blocker.remove());
+    lockedElement.style.opacity = '1';
+    lockedElement.classList.remove('lock');
+    return clearInterval(intervalFunc);
+  }
+
+  tryCount++;
+}
+
+if (window.location.host.indexOf("clarin.com") !== -1) {
+  console.log('Diarios Sin Interrupciones, Running!');
+  intervalFunc = setInterval(checkClarinModalVisibility, 1000);
+}
---
+++ popup/src/scripts/components/app/App.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Muchas Gracias por utilizar esta extension!
+      </div>
+    );
+  }
+}
+
+export default App;
